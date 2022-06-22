d9cd38f646c2d27cec72a5980e4263ea4b2a45fe
Oghenemeru G. Avwemoya <40310595+skiboss@users.noreply.github.com>
2022-06-22 22:29:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("Introduction to JavaScript!");
+alert("Name: Oghenemeru Avwemoya");
+alert("Height: 5.9 ");
+alert("Country: Nigeria");
+
+console.log("Name: Oghenemeru Avwemoya");
+console.log("Height: 5.9 ");
+console.log("Country: Nigeria");
